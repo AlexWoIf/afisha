f3d549a0ac421399fd721ecd4d5279176fec7862
AlexWoIf <alexey.barannikov@gmail.com>
2023-12-22 22:54:26 +0300

    add images loading at place admin page

--- places/admin.py
+++ places/admin.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from places.models import Place, Image
 
 
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 1
+
 @admin.register(Place)
 class Place(admin.ModelAdmin):
     list_display = ('title', )
+    inlines = [ImageInline]
 
 
 admin.site.register(Image)
--- places/views.py
+++ places/views.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
