8a31876291a9310395bc8476f9e47e6895b0b0f3
AlexWoIf <alexey.barannikov@gmail.com>
2023-12-25 18:17:50 +0300

    make some fields  unnecessary

--- places/models.py
+++ places/models.py
@@ -3,9 +3,9 @@ from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-    title = models.CharField('Заголовок', max_length=200)
-    short_description = models.TextField('Краткое описание')
-    long_description = HTMLField('Полное описание')
+    title = models.CharField('Заголовок', max_length=200, )
+    short_description = models.TextField('Краткое описание', blank=True, )
+    long_description = HTMLField('Полное описание', blank=True, )
     lng = models.FloatField('Долгота')
     lat = models.FloatField('Широта')
 
